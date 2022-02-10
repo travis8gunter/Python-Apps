28f24cd9079636cad4b3b4cc2f0b7c5b7faae0a3
Travis Gunter <53490233+travis8gunter@users.noreply.github.com>
2022-02-09 23:41:05 -0700

    Notes
    More notes

---
+++ PyNotes/TESTER.py
@@ -0,0 +1,34 @@
+from cisc108 import assert_equal
+def double(num,num2)*2:
+    
+    '''
+THIS IS A EXAMPLE OF A DOCUMENT STRING
+    This is a fucntion that takes two number and multipoles them together
+    
+    ARGS:
+    num(float)
+    num2(float) these are the two number
+    
+    RETURNS:
+    (float) the answer
+    '''
+    return num*num2*2
+
+assert_equal(double(2,2),8)
+assert_equal(double(2,2)12)
+
+
+def load_student():
+    '''
+    Brings in a student
+    
+    ARGS:
+    name(str) name of the student 
+    student_ID(str) name of the student ID
+    
+    
+    RETURNS:
+    latest_submision (string) latest student submission
+    '''
+    
+    
