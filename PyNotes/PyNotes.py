a47e53a5ed7d966673e244bb36872a494bbde97d
Travis Gunter <53490233+travis8gunter@users.noreply.github.com>
2022-02-09 23:16:48 -0700

    Notes
    Dummy notes in python

---
+++ PyNotes/PyNotes.py
@@ -0,0 +1,32 @@
+# Get user's name
+name = input("What is your name?")
+# Print a welcome message
+print("Welcome", name)
+# Get user's age; input returns a string
+age = input("How old are you?")
+# Convert string to integer using built-in int function
+# The int function turns strings into integers - we'll learn more about it soon
+age = int(age)
+# Calculate age in dog year's
+dog_years = age * 7
+# Print result
+print("You are", dog_years, "in dog years!")
+
+#--------------------------------------------------------------------
+print("""Welcome to the highway travel advisor.
+This app is made to work with travel on I-15 in Utah.
+We will ask some questions than give you travel advice!""")
+
+mile = input("What I-15 Mile Marker will you Enter on? ")
+end = input("What I-15 Mile Marker will you Exit on? ")
+time = input("How many hours from now do you want to arrive? ")
+speed = input("What will be the average MPH you go? ")
+
+int(dis = end - mile)
+float(times = dis/speed)
+
+
+
+print("You will travel, ",dis,"miles")
+print("You should leave,")
+print("Thanks for using our service!!")
