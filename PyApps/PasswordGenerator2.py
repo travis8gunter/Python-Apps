92ccdf681cff9ab9f210b7074ee75443714088e8
Travis Gunter <53490233+travis8gunter@users.noreply.github.com>
2022-03-27 19:22:59 -0600

    Password Generator Python
    Password generator py file

---
+++ PyApps/PasswordGenerator2.py
@@ -0,0 +1,14 @@
+import string
+import random
+
+s1= string.ascii_lowercase
+s2 = string.ascii_uppercase
+s3 = string.digits
+s4 = string.punctuation
+plen = int(input("Enter Password Length: "))
+s = []
+s.extend(list(s1))
+s.extend(list(s2))
+s.extend(list(s3))
+s.extend(list(s4))
+print("".join(random.sample(s, plen)))
